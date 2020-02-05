1412e591ae79a9e8b10f7928cbb0aeab3fe13f4a
Ian Andrewson <ianandrewson@gmail.com>
2020-02-05 11:51:14 -0800

    main component skeleton implemented. snapshot updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import Header from '../components/Header/Header.js';
+import Main from '../components/Main/Main.js';
 
 export default function App() {
   return (
-    <Header />
+    <>
+      <Header />
+      <Main />
+    </>
   );
 }
---
+++ src/components/Main/Main.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function Main() {
+  return(
+    <>
+      <h1>FILLER</h1>
+    </>
+  );
+}
